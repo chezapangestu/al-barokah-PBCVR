0604e921ca4bee6d0c8963fdc4fc55756c76afee
vijaycheza <cheza426@gmail.com>
2023-05-18 20:48:42 +0700

    update image syuruq

--- pages/waktuSyuruq/index.jsx
+++ pages/waktuSyuruq/index.jsx
@@ -21,22 +21,22 @@ export default function WaktuSyuruq() {
   //   }, [])
 
   return (
-    <div className="syuruq-main px-72 py-56">
+    <div className="syuruq-main px-72 py-60">
       <Alarm ref={alarmRef} />
-      <div className="flex flex-col space-y-10 bg-black opacity-80 text-white rounded-lg p-10">
-        <p className="font-medium text-xl">
+      <div className="flex flex-col space-y-10 bg-black opacity-75 text-white rounded-lg p-10">
+        <p className="font-medium text-2xl">
           Dari Abu Umamah, Rasulullah shallallahu ‘alaihi wa sallam bersabda,
         </p>
-        <h1 className="text-3xl font-bold select-none text-center">
+        <h1 className="text-4xl font-bold select-none text-center">
           مَنْ صَلَّى صَلاةَ الصُّبْحِ فِي مَسْجِدِ جَمَاعَةٍ يَثْبُتُ فِيهِ
           حَتَّى يُصَلِّيَ سُبْحَةَ الضُّحَى، كَانَ كَأَجْرِ حَاجٍّ، أَوْ
           مُعْتَمِرٍ تَامًّا حَجَّتُهُ وَعُمْرَتُهُ
         </h1>
-        <p className="font-medium text-2xl">
+        <p className="font-medium text-3xl text-justify">
           “Barangsiapa yang mengerjakan shalat shubuh dengan berjama’ah di
           masjid, lalu dia tetap berdiam di masjid sampai melaksanakan shalat
           sunnah Dhuha, maka ia seperti mendapat pahala orang yang berhaji atau
-          berumroh secara sempurna.”3
+          berumroh secara sempurna.”
         </p>
       </div>
     </div>
