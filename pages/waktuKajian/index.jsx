cdf11d0a8012de3b58ce4adbc64ee0b238af0db3
vijaycheza <cheza426@gmail.com>
2023-05-21 06:18:59 +0700

    add kajian display screen saver

---
+++ pages/waktuKajian/index.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import thumbnail from '../../public/pict/Thumbnail 1920x1080.jpg'
+
+export default function WaktuKajian() {
+  return (
+    <div>
+      <Image src={thumbnail} alt="banner-kajian"></Image>
+    </div>
+  )
+}
