da4fe84cb82e983665529689bd20213c0ba68d5e
vijaycheza <cheza426@gmail.com>
2023-05-21 06:28:51 +0700

    update height display waktuKajian from 810 to 900px

--- pages/waktuKajian/index.jsx
+++ pages/waktuKajian/index.jsx
@@ -4,7 +4,7 @@ import thumbnail from '../../public/pict/Thumbnail 1920x1080.jpg'
 export default function WaktuKajian() {
   return (
     <div>
-      <Image src={thumbnail} alt="banner-kajian" height={810}></Image>
+      <Image src={thumbnail} alt="banner-kajian" height={900}></Image>
     </div>
   )
 }
